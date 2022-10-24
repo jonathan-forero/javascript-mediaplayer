0fb75e12c1302a2cf6827147c7915c16a663ce6a
jonathan-forero <forerojonathan@gmail.com>
2022-10-24 13:09:38 -0500

    add: AutoPlay plugin

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,10 @@
 import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
+const buttonPlay = document.querySelector("#play");
+const buttonMute = document.querySelector("#mute");
 
-const player = new MediaPlayer({ el: video });
-button.onclick = () => player.togglePlay();
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+buttonPlay.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMute();
